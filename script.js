aa5316a47d8efac05158b2d5aafc54d2a1e99fb1
Jack Hayley <66978120+Andethy@users.noreply.github.com>
2023-10-12 18:52:50 -0400

    Added output lines

--- script.js
+++ script.js
@@ -39,6 +39,7 @@ let outputLine = (line) => {
 let addMovie = (movie) => {
     allMovies.push(movie);
     console.log("A new movie is added");
+    outputLine("A new movie is added");
 }
 
 //iterate through all elements of allMovies array
@@ -50,31 +51,39 @@ let printMovies = () => {
 
     for (let i = 0; i < allMovies.length; i++) {
         console.log(allMovies[i].title + ", rating of " + allMovies[i].rating + ", havewatched: " + allMovies[i].haveWatched);
+        outputLine(allMovies[i].title + ", rating of " + allMovies[i].rating + ", havewatched: " + allMovies[i].haveWatched);
     }
     console.log("");
+    outputLine("");
     console.log("You have " + allMovies.length + " movies in total");
+    outputLine("You have " + allMovies.length + " movies in total");
 }
 
 
 //Display only the movies that has a rating higher than rating(argument)
 //Display the total number of matches
 let highRatings = (rating) => {
-    console.log("printing movies that have a rating higher than " + rating);
+    console.log("Printing movies that have a rating higher than " + rating);
+    outputLine("Printing movies that have a rating higher than " + rating)
     let count = 0;
     for (let i = 0; i < allMovies.length; i++) {
         if (allMovies[i].rating > rating) {
             console.log(allMovies[i].title + " has a rating of " + allMovies[i].rating);
+            outputLine(allMovies[i].title + " has a rating of " + allMovies[i].rating);
             count++;
         }
     }
     console.log("");
+    outputLine("");
     console.log("In total, there are " + count + " matches");
+    outputLine("In total, there are " + count + " matches");
 }
 
 
 //Toggle the 'haveWatched' property of the specified movie 
 let changeWatched = (title) => {
     console.log("changing the status of the movie...");
+    outputLine("changing the status of the movie...");
     for (let i = 0; i < allMovies.length; i++) {
         if (allMovies[i].title === title) {
             allMovies[i].haveWatched = !(allMovies[i].haveWatched);
@@ -94,8 +103,11 @@ allMovies.push(x,y,z);
 
 /*replace console.log with display on web page*/
 console.log("----------------");
+outputLine("----------------");
 console.log("running program......");
+outputLine("running program......");
 console.log("----------------");
+outputLine("----------------");
 printMovies();
 
 
@@ -103,26 +115,32 @@ let movie1 = new Movie("Parasite", 2, false);
 
 /*replace console.log with display on web page*/
 console.log("----------------");
+outputLine("----------------");
 addMovie(movie1);
 console.log("----------------");
+outputLine("----------------");
 
 
 
 changeWatched("Spiderman");
 /*replace console.log with display on web page*/
 console.log("----------------");
+outputLine("----------------");
 
 printMovies();
 
 /*replace console.log with display on web page*/
 console.log("----------------");
+outputLine("----------------");
 
 changeWatched("Spiderman");
 /*replace console.log with display on web page*/
 console.log("----------------");
+outputLine("----------------");
 
 printMovies();
 /*replace console.log with display on web page*/
 console.log("----------------");
+outputLine("----------------");
 
 highRatings(3.5);
