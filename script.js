6140220144b0c337271a254615b2708ecaff360e
Jack Hayley <66978120+Andethy@users.noreply.github.com>
2023-10-12 16:33:15 -0400

    Completed script

--- script.js
+++ script.js
@@ -2,31 +2,58 @@ let allMovies = [];
 
 //Define a movie class with parameters title (string), rating (number) and haveWatched (boolean)
 class Movie {
+    constructor(title, rating, haveWatched) {
+        this.title = title;
+        this.rating = rating;
+        this.haveWatched = haveWatched;
+    }
 
 }
 
 //add a movie OBJECT to the allMovies array
 let addMovie = (movie) => {
-
+    allMovies.push(movie);
+    console.log("A new movie is added");
 }
 
 //iterate through all elements of allMovies array
 //Display the total number of movies in allMovies array
 let printMovies = () => {
+    console.log("Printing all movies....");
 
+
+    for (let i = 0; i < allMovies.length; i++) {
+        console.log(allMovies[i].title + ", rating of " + allMovies[i].rating + ", havewatched: " + allMovies[i].haveWatched);
+    }
+    console.log("");
+    console.log("You have " + allMovies.length + " movies in total");
 }
 
 
 //Display only the movies that has a rating higher than rating(argument)
 //Display the total number of matches
 let highRatings = (rating) => {
-
+    console.log("printing movies that have a rating higher than " + rating);
+    let count = 0;
+    for (let i = 0; i < allMovies.length; i++) {
+        if (allMovies[i].rating > rating) {
+            console.log(allMovies[i].title + " has a rating of " + allMovies[i].rating);
+            count++;
+        }
+    }
+    console.log("");
+    console.log("In total, there are " + count + " matches");
 }
 
 
 //Toggle the 'haveWatched' property of the specified movie 
 let changeWatched = (title) => {
-
+    console.log("changing the status of the movie...");
+    for (let i = 0; i < allMovies.length; i++) {
+        if (allMovies[i].title === title) {
+            allMovies[i].haveWatched = !(allMovies[i].haveWatched);
+        }
+    }
 }
 
 
